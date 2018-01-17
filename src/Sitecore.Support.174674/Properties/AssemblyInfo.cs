d7e6c8ee2e233b74eb6e88ebf56225a065838ab5
DK\AMCH <amch@sitecore.net>
2018-01-17 15:59:07 +0200

    initial commit

---
+++ src/Sitecore.Support.174674/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.174674")]
+[assembly: AssemblyProduct("Sitecore.Support.174674")]
+[assembly: ComVisible(false)]
